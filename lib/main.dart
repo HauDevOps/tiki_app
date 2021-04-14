4e6fdadf557754d037451e8093577f34f53eecc5
Hau <hau.tran@eton.vn>
2021-04-14 17:38:53 +0700

    [Ver 1.3] Update to SliverAppbar

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       onGenerateRoute: CustomerRouter.allRoutes,
       initialRoute: router_api,
     );
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -6,6 +6,7 @@ import 'package:tiki_app/entity/deal_hot_entity.dart';
 import 'package:tiki_app/entity/quick_link_entity.dart';
 import 'package:tiki_app/pages/home/home_bloc.dart';
 
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -27,14 +28,21 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue[300],
       body: CustomScrollView(
         slivers: [
           _appBarWidget(),
-          // SliverFixedExtentList(
-          //   itemExtent: 230,
-          //   delegate: SliverChildListDelegate([_bannerWidget(bloc)]),
-          // ),
-          _dealHotWidget(bloc),
+          SliverFixedExtentList(
+            itemExtent: 150,
+            delegate: SliverChildListDelegate([_bannerWidget(bloc)]),
+          ),
+          SliverFixedExtentList(
+            itemExtent: 180,
+            delegate: SliverChildListDelegate([_dealHotWidget(bloc)]),
+          ), SliverFixedExtentList(
+            itemExtent: 240,
+            delegate: SliverChildListDelegate([_quickLinkWidget(bloc)]),
+          ),
           // _quickLinkWidget(bloc)
         ],
       ),
@@ -45,6 +53,7 @@ class _HomePageState extends State<HomePage> {
     return SliverAppBar(
       floating: false,
       expandedHeight: 50,
+      backgroundColor: Colors.blue[300],
       flexibleSpace: FlexibleSpaceBar(
         title: Text(
           'TIKI',
@@ -56,27 +65,63 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _quickLinkWidget(HomeBloc bloc) {
-    return StreamBuilder(
-      stream: bloc.quickLinkStream,
-      builder: (BuildContext context, AsyncSnapshot<QuickLink> snapShot) {
-        if (!snapShot.hasData) {
-          return Center(
-            child: Text('Data not found'),
-          );
-        }
-        return ListView.builder(
-          itemCount: snapShot.data.data.length,
-          itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                onTap: () {},
-                leading: Image.network(snapShot.data.data[index][0].imageUrl),
-              ),
+    return Container(
+      color: Colors.white,
+      child: StreamBuilder(
+        stream: bloc.quickLinkStream,
+        builder: (BuildContext context, AsyncSnapshot<QuickLink> snapShot) {
+          if (!snapShot.hasData) {
+            return Center(
+              child: Text('Data not found'),
             );
-          },
-        );
-      },
+          }
+          return ListView.builder(
+            itemCount: snapShot.data.data.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(5),
+                child: Container(
+                  height: 100,
+                  width: 50,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: snapShot.data.data[index].length,
+                    itemBuilder: (context, i) {
+                      return Container(
+                        margin: const EdgeInsets.all(7),
+                          child: Column(
+                                children: [
+                                  Image.network(snapShot.data.data[index][i].imageUrl, height: 50, width: 50,),
+                                  Text(snapShot.data.data[index][i].title, textAlign: TextAlign.center,),
+                                ],
+                          ),
+                      );
+                    },
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
+  }
+
+  Widget _bottomWidget(){
+    return BottomNavigationBar(items: const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'Trang chủ',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.table_chart),
+        label: 'Danh mục',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.fireplace),
+        label: 'Lướt',
+      ),
+    ],);
   }
 
   Widget _bannerWidget(HomeBloc bloc) {
@@ -98,17 +143,17 @@ class _HomePageState extends State<HomePage> {
           items: snapShot.data.map((i) {
             return Builder(
               builder: (BuildContext context) {
-                return Container(
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(20)),
-                  child: Center(
+                return Center(
+                    child: ClipRRect(
+                      borderRadius:
+                      BorderRadius.circular(15.0),
                       child: Image.network(
-                    i.imageUrl,
-                    fit: BoxFit.fill,
-                    width: 420,
-                    height: MediaQuery.of(context).size.width,
-                  )),
-                );
+                        i.imageUrl,
+                        fit: BoxFit.fill,
+                        width: 300,
+                        height: 140,
+                      ),
+                    ));
               },
             );
           }).toList(),
@@ -118,30 +163,31 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _dealHotWidget(HomeBloc bloc) {
-    return StreamBuilder(
-      stream: bloc.dealHotStream,
-      builder:
-          (BuildContext context, AsyncSnapshot<List<Data>> snapShot) {
-        if (!snapShot.hasData) {
-          return Center(
-            child: Text('Data not found'),
-          );
-        }
-        return Container(
-          color: Colors.grey,
-          height: 260,
-          child: Expanded(
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-              itemCount: snapShot.data.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        child: Stack(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            titleDealHot(),
+            StreamBuilder(
+              stream: bloc.dealHotStream,
+              builder:
+                  (BuildContext context, AsyncSnapshot<List<Data>> snapShot) {
+                if (!snapShot.hasData) {
+                  return Center(
+                    child: Text('Data not found'),
+                  );
+                }
+                return Expanded(
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.all(5),
+                    itemCount: snapShot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Column(
                           children: [
                             ClipRRect(
                               borderRadius:
@@ -149,54 +195,112 @@ class _HomePageState extends State<HomePage> {
                               child: Image.network(
                                 snapShot.data[index].product
                                     .thumbnailUrl,
-                                height: 150.0,
-                                width: 150.0,
+                                height: 100,
+                                width: 100,
                                 fit: BoxFit.cover,
+                              ),
+                            ),
+                            Text(
+                              snapShot.data[index].product.price
+                                  .toString(),
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red),
+                            ),
+                            ClipRRect(
+                              borderRadius:
+                              BorderRadius.circular(5),
+                              child: Text(
+                                'Đã bán ' +
+                                    snapShot.data[index].progress
+                                        .qtyOrdered
+                                        .toString(),
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    backgroundColor:
+                                    Colors.red[600]),
                               ),
                             ),
                           ],
                         ),
-                      ),
-                      Stack(
-                        children: [
-                          Text(
-                            snapShot.data[index].product.price
-                                .toString(),
-                            style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.red),
-                          ),
-                        ],
-                      ),
-                      Stack(
-                        children: [
-                          ClipRRect(
-                            borderRadius:
-                            BorderRadius.circular(5),
-                            child: Text(
-                              'Đã bán ' +
-                                  snapShot.data[index].progress
-                                      .qtyOrdered
-                                      .toString(),
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                  backgroundColor:
-                                  Colors.red[600]),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
+                      );
+                    },
                   ),
                 );
               },
             ),
-          ),
-        );
-      },
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget titleDealHot() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Giá Sốc / Hôm Nay',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 17,
+              color: Colors.deepOrange),
+        ),
+        TweenAnimationBuilder<Duration>(
+          duration: Duration(minutes: 60),
+          tween: Tween(begin: Duration(minutes: 120), end: Duration.zero),
+          onEnd: () {},
+          builder: (BuildContext context, Duration value, Widget child) {
+            final minutes = value.inMinutes % 60;
+            final seconds = value.inSeconds % 60;
+            final hour = value.inHours % 24;
+            return Row(
+              children: [
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child: Text(
+                      "$hour",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Colors.white,
+                          backgroundColor: Colors.red),
+                    )),
+                Text(" : "),
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child: Text(
+                      "$minutes",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Colors.white,
+                          backgroundColor: Colors.red),
+                    )),
+                Text(" : "),
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child: Text(
+                      "$seconds",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Colors.white,
+                          backgroundColor: Colors.red),
+                    )),
+              ],
+            );
+          },
+        ),
+        // Text(
+        //   '>',
+        //   style: TextStyle(
+        //       fontWeight: FontWeight.bold, fontSize: 20, color: Colors.grey),
+        // ),
+      ],
     );
   }
 }
